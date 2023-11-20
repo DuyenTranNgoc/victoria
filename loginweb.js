a9529ff3c06fcb8dd0eb0136668c6578b900b7d4
DuyenTranNgoc <151430930+DuyenTranNgoc@users.noreply.github.com>
2023-11-20 15:39:53 +0700

    trang chủ

---
+++ loginweb.js
@@ -0,0 +1,135 @@
+let demo=false
+let demo1=false
+let demo2=false
+let demo3=false
+let demo4=false
+let demo5=false
+let demo6=false
+let demo7=false
+let demo8=false
+
+document.getElementById("demo").onclick = function() {myFunction0()};
+
+function myFunction0() {
+  if (demo){
+    demo=false;
+    document.getElementById("demo").innerHTML = "❤️";
+  }
+  else{
+    demo=true;
+    document.getElementById("demo").innerHTML = "🤍";
+  }
+ 
+}
+
+document.getElementById("demo1"). onclick = function() {myFunction1()};
+
+function myFunction1() {
+  if (demo1){
+    demo1=false;
+    document.getElementById("demo1").innerHTML = "❤️";
+  }
+  else{
+    demo1=true;
+    document.getElementById("demo1").innerHTML = "🤍";
+  }
+}
+
+document.getElementById("demo2"). onclick = function() {myFunction2()};
+
+function myFunction2() {
+  if (demo2){
+    demo2=false;
+    document.getElementById("demo2").innerHTML = "❤️";
+  }
+  else{
+    demo2=true;
+    document.getElementById("demo2").innerHTML = "🤍";
+  }
+}
+
+document.getElementById("demo3").onclick = function() {myFunction3()};
+
+function myFunction3() {
+  if (demo3){
+    demo3=false;
+    document.getElementById("demo3").innerHTML = "❤️";
+  }
+  else{
+    demo3=true;
+    document.getElementById("demo3").innerHTML = "🤍";
+  }
+}
+
+document.getElementById("demo4").onclick = function() {myFunction4()};
+
+function myFunction4() {
+  if (demo4){
+    demo4=false;
+    document.getElementById("demo4").innerHTML = "❤️";
+  }
+  else{
+    demo4=true;
+    document.getElementById("demo4").innerHTML = "🤍";
+  }
+}
+
+document.getElementById("demo5"). onclick = function() {myFunction5()};
+
+function myFunction5() {
+  if (demo5){
+    demo5=false;
+    document.getElementById("demo5").innerHTML = "❤️";
+  }
+  else{
+    demo5=true;
+    document.getElementById("demo5").innerHTML = "🤍";
+  }
+}
+
+document.getElementById("demo6"). onclick = function() {myFunction6()};
+
+function myFunction6() {
+  if (demo6){
+    demo6=false;
+    document.getElementById("demo6").innerHTML = "❤️";
+  }
+  else{
+    demo6=true;
+    document.getElementById("demo6").innerHTML = "🤍";
+  }
+}
+
+document.getElementById("demo7"). onclick = function() {myFunction7()};
+
+function myFunction7() {
+  if (demo7){
+    demo7=false;
+    document.getElementById("demo7").innerHTML = "❤️";
+  }
+  else{
+    demo7=true;
+    document.getElementById("demo7").innerHTML = "🤍";
+  }
+}
+
+document.getElementById("demo8"). onclick = function() {myFunction8()};
+
+function myFunction8() {
+  if (demo8){
+    demo8=false;
+    document.getElementById("demo8").innerHTML = "❤️";
+  }
+  else{
+    demo8=true;
+    document.getElementById("demo8").innerHTML = "🤍";
+  }
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
